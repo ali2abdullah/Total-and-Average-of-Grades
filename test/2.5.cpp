6b4525723dfe6d99e08c0250d9f2c7d88fbb816b
ali2abdullah <ali561209@gmail.com>
2023-05-28 18:16:31 +0300

    a C++ program to find the Total and Average of the giving grades

---
+++ test/2.5.cpp
@@ -0,0 +1,25 @@
+//Ali Abdullah Al Omani
+//431900303
+
+#include <iostream>
+    using namespace std;
+    
+int main()
+{
+    double g1, g2, g3, g4, g5, g6, total, averge;
+    g1 = 54.5;
+    g2 = 45.5;
+    g3 = 40.0;
+    g4 = 24.5;
+    g5 = 65.5;
+    g6 = 10.0;
+    
+    total = g1 + g2 + g3 + g4 + g5 + g6;
+    averge = total / 6.0;
+    
+    cout << "The total is = " <<total 
+         <<"\nThe averge is = " <<averge;
+    
+    return 0;
+}
+
